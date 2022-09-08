99aeda9453cc35b51fbf398b13add7e485832d46
arsitta <sitta93@gmail.com>
2022-09-08 05:05:21 -0300

    ADD: Configuraciones para heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path');
 
 const app = express();
-const port = 3002;
+const port = 3000;
 const publicPath = path.join(__dirname, '/public'); 
 
 app.use(express.static(publicPath));
@@ -11,6 +11,6 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/home.html');
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Servidor corriendo en puerto ${port}`)
 })
